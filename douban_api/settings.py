7325a5bedb42e8bdbe54065dba04e6ccef6e67c1
yanpan9 <1335624722a@gmail.com>
2020-04-22 11:30:20 +0800

    Update settings

--- douban_api/settings.py
+++ douban_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=4ftzuz7s##(1zpch$ng+v3fy)u-rys%6adzevj(%z(pd_x23#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.cnyanpan.top"]
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
